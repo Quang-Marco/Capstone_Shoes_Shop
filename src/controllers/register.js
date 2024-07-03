4ec6bd51340d141c25e1e28ae67347c3c5648ca0
Quang - Marco <trannhanthienquang1803@gmail.com>
2024-07-04 03:13:18 +0700

    finish popup & dark theme

--- src/controllers/register.js
+++ src/controllers/register.js
@@ -3,3 +3,8 @@ function toggleLogin() {
   document.getElementById("register").style.display = "none";
   document.getElementById("sign_in").style.display = "block";
 }
+
+function toggleRegister() {
+  document.getElementById("register").style.display = "block";
+  document.getElementById("sign_in").style.display = "none";
+}
